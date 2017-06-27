675064e2e776941ebc751a6da3855040083511ed
shihaab <roushi02@gmail.com>
2017-06-27 16:25:16 +0200

    translated

--- login/index.php
+++ login/index.php
@@ -63,12 +63,12 @@ $pdo = ConnectDB();
         $check = login($gebruikersnaam,$wachtwoord,$pdo);
         if ($check){
 			header('Refresh:5;url=https://tombstone-squad.com');
-			echo '<h1 class="text-center" style="margin-top:50px;color:white;">U bent succesvol ingelogd</h1>';
+			echo '<h1 class="text-center" style="margin-top:50px;color:white;">Login successfull</h1>';
 			echo '<div id="loader"><div id="top"></div><div id="bottom"></div><div id="line"></div></div>';
         }
         else{
 			header('Refresh:5;url=https://tombstone-squad.com');
-			echo '<h1 class="text-center" style="margin-top:50px;color:white;">De Inlognaam of het paswoord is onjuist.</h1>';
+			echo '<h1 class="text-center" style="margin-top:50px;color:white;">Username or password is incorrect</h1>';
         }
 
     }
